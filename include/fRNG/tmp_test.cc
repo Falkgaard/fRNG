1c2ce235f8113296608ce53e46687b0e7221b66a
Falkgaard <falkgaard@gmail.com>
2021-11-20 09:57:35 +0100

    First commit

--- include/fRNG/tmp_test.cc
+++
@@ -1,7 +0,0 @@
-#include "falk_rng.hh"
-#include <cstdio>
-int main() {
-	rng::distributions::uniform random_dist( 50., .25 );
-	for ( int i=5; i-->0; )
-		std::printf( "%g\n", rng::get(random_dist) );
-}
